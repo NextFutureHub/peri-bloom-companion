e7dec2f1c0681d7d40ff13681fafba0989040001
NextFutureHub <nextfuture.pvl@gmail.com>
2025-11-10 19:21:19 +0500

    rename import toaster and sonner

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -7,8 +7,8 @@ export * from "./atoms/textarea";
 export * from "./atoms/select";
 export * from "./atoms/dialog";
 export * from "./atoms/toast";
-export * from "./atoms/toaster";
-export * from "./atoms/sonner";
+export { Toaster as ShadcnToaster } from "./atoms/toaster";
+export { Toaster as SonnerToaster, toast } from "./atoms/sonner";
 export * from "./atoms/tooltip";
 
 
